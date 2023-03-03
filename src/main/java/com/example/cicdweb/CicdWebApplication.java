b7836ef38928fbc61c5e9793cbede64e9c133a59
kim jisu <jisu3316@naver.com>
2023-03-04 01:00:55 +0900

    init

---
+++ src/main/java/com/example/cicdweb/CicdWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cicdweb/CicdWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
